5bdf8a8a3df7a729c22ad833a7ded460caebc3b7
Darya <HavrylenkoIT@gmail.com>
2022-11-18 13:38:15 +0200

    start edit task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { Movies } from '../pages/Movies';
+import { NotFound } from '../pages/NotFound';
+import { Link } from './App.styled';
+
 export const App = () => {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movies</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+  return (
+    <main>
+      <h1>Trending today</h1>
+      <ul>
+        <li></li>
+      </ul>
+    </main>
+  );
+};
