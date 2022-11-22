e66eb42806396afb24028d0134bcc9f1934d56ee
Darya <HavrylenkoIT@gmail.com>
2022-11-22 23:47:40 +0200

    fix error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getTrendingFilms } from '../servises/api';
 import { MoviesList } from 'components/MoviesList/MoviesList';
-import styled from 'styled-components';
-
-// const MainHome = styled.div`
-//   background: rgb(34, 193, 195);
-//   background: radial-gradient(
-//     circle,
-//     rgba(34, 193, 195, 1) 0%,
-//     rgba(253, 187, 45, 1) 100%
-//   );
-// `;
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
